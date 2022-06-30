806b7a88aa40dc17cb2461d901d2e7e248096e45
avichai321 <77723644+avichai321@users.noreply.github.com>
2022-06-30 21:42:50 +0300

    update

--- שיעור 3/hw3/src/Urlcomponents.jsx
+++ שיעור 3/hw3/src/Urlcomponents.jsx
@@ -1,61 +1,106 @@
 
 import React, { useState } from "react";
 
-
-
-export function ShowAndAddTolist()
+ class UrlList extends React.Component
 {
-    let Urllist=    [  
-        {url : 'https://www.youtube.com/watch?v=ON8xpczPZAU'},
-        {url :'https://www.youtube.com/watch?v=V3r-YGGsKpk'},
-        {url :'https://www.youtube.com/watch?v=yCX7y6M9RUg'},
-        {url :'https://www.youtube.com/watch?v=yOykfhhXDXM&t=2261s'}          
-    ]
+    
+    constructor(props){
+        super(props)
+        this.state = {urls: urlLinksList()};
+    }
 
-    const [UrlLinkList, SetUrlLinkList] = useState([ {url : 'https://www.youtube.com/watch?v=ON8xpczPZAU'},
-    {url :'https://www.youtube.com/watch?v=V3r-YGGsKpk'},
-    {url :'https://www.youtube.com/watch?v=yCX7y6M9RUg'},
-    {url :'https://www.youtube.com/watch?v=yOykfhhXDXM&t=2261s'}]);
+    AddUrlClick() {
+        let nurl= this.state.value
+        let tempstate = [...this.state.urls,nurl]
+        console.log(tempstate)
+        this.setState({urls: tempstate})
+    };
+
+
+    // handleChange(e){
+    //     let x = this.state.value
+    //     this.setState({valuef : x})
+    //   }
+    
+    render() {
+        return(
+            <div>
+                <h2>Url list</h2>
+                 {/* <input type="text" value={this.state.value} placeholder="Enter url" onChange={this.handleChange}/>  */}
+                 {/* <button onClick={this.AddUrlClick}>add new url</button>  */}
+            
+            <ul>
+                {this.state.urls.map(urls => {
+                    return <li>{urls}</li>;
+                })}
+            </ul>  
+                
+                {/* <button onClick={this.clearAll}>Erase everything</button> */}
+            </div>);
+            };
+    
+
+    
+}
+export default UrlList
+
+function urlLinksList(){
+    return(  [  
+        'https://www.youtube.com/watch?v=ON8xpczPZAU',
+        'https://www.youtube.com/watch?v=V3r-YGGsKpk',
+        'https://www.youtube.com/watch?v=yCX7y6M9RUg',
+        'https://www.youtube.com/watch?v=yOykfhhXDXM&t=2261s'        
+    ]
+    )
+}
+
+//  function ShowAndAddTolist()
+// {
+   
+//     const [UrlLinkList, SetUrlLinkList] = useState([ {url : 'https://www.youtube.com/watch?v=ON8xpczPZAU'},
+//     {url :'https://www.youtube.com/watch?v=V3r-YGGsKpk'},
+//     {url :'https://www.youtube.com/watch?v=yCX7y6M9RUg'},
+//     {url :'https://www.youtube.com/watch?v=yOykfhhXDXM&t=2261s'}]);
                                 
 
-        const AddUrlClick = () => {
-            SetUrlLinkList([...UrlLinkList, {url: ""}]);
-          };
-        const changeInputclick = (e, index) => {
-            const { name , value} = e.target;
-            const listurl = [...UrlLinkList];
-            listurl[index][name] = value;
-            SetUrlLinkList(listurl);
-        };
-        const RemoveUrlClick = index => {
-            const listurl = [...UrlLinkList];
-            listurl.splice(index, 1);
-            SetUrlLinkList(listurl);
-          };
-            return (
-            <div >
-                {UrlLinkList.map((x,i) => {
-                return (
-                    <div>
-                    <input name="url" value={x.url} placeholder="Enter url" onChange={e => changeInputclick(e,i)}/>
-                    <div>
-                        {UrlLinkList.length !== 1 && <button onClick={RemoveUrlClick}>Remove</button>}
-                        {UrlLinkList.length - 1 === i && <button onClick={AddUrlClick} >Add</button>}
-                    </div>
-                    </div>
-                );
-                })}
-                <div style={{ marginTop: 20 }}>{JSON.stringify(UrlLinkList)}</div>
+//         const AddUrlClick = () => {
+//             SetUrlLinkList([...UrlLinkList, {url: ""}]);
+//           };
+//         const changeInputclick = (e, index) => {
+//             const { name , value} = e.target;
+//             const listurl = [...UrlLinkList];
+//             listurl[index][name] = value;
+//             SetUrlLinkList(listurl);
+//         };
+//         const RemoveUrlClick = index => {
+//             const listurl = [...UrlLinkList];
+//             listurl.splice(index, 1);
+//             SetUrlLinkList(listurl);
+//           };
+//             return (
+//             <div >
 
-            </div>
+//                 {UrlLinkList.map((x,i) => {
+//                 return (
+//                     <div>
+                        
+//                     <input name="url" value={x.url} placeholder="Enter url" onChange={e => changeInputclick(e,i)}/>
+//                     <div>
+//                         {UrlLinkList.length !== 1 && <button onClick={RemoveUrlClick}>Remove</button>}
+//                         {UrlLinkList.length - 1 === i && <button onClick={AddUrlClick} >Add</button>}
+//                     </div>
+//                     </div>
+//                 );
+//                 })}
+//             </div>
 
         
 
 
-      );
+//       );
     
      
-};
+// };
 
 
   
--- שיעור 3/hw3/src/index.js
+++ שיעור 3/hw3/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ShowAndAddTolist } from './Urlcomponents';
+import UrlList from './Urlcomponents';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<ShowAndAddTolist></ShowAndAddTolist>
+<UrlList></UrlList>
 );
 
 
